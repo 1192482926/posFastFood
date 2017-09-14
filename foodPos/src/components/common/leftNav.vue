41516840aaf2c4f2bc77415e353628e19811f965
1192482926 <13666154341@163.com>
2017-09-14 17:48:24 +0800

    111

---
+++ foodPos/src/components/common/leftNav.vue
@@ -0,0 +1,61 @@
+<template>
+	<div class="leftnav">
+		<ul>
+			 <li>
+	            <i class="icon iconfont icon-zhuanzhang"></i>
+	            <div>收银</div>
+	        </li> 
+	        <li>
+	            <i class="icon iconfont icon-shouye2"></i>
+	            <div>店铺</div>
+	        </li>
+	 
+	        <li>
+	            <i class="icon iconfont icon-shouye3"></i>
+	            <div>商品</div>
+	        </li>
+	 
+	         <li>
+	            <i class="icon iconfont icon-shouye1"></i>
+	            <div>会员</div>
+	        </li>
+	 
+	 
+	        <li>
+	            <i class="icon iconfont icon-caiwu"></i>
+	            <div>统计</div>
+	        </li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default ({
+		name: 'leftnav'
+	})
+</script>
+
+<style>
+	*{margin: 0;padding: 0;}
+	.leftnav{
+       color:#fff;
+       font-size:10px;
+       height:100%;
+       background-color: #1D8ce0;
+       float:left;
+       width:5%;
+    }
+    .iconfont{
+       font-size:24px;
+    }
+    .leftnav ul{
+        padding:0px;
+        margin: 0px;
+    }
+    .leftnav li{
+        list-style: none;
+        text-align: center;
+        border-bottom:1px solid #EFF2F7;
+        padding:10px;
+    }
+</style>
---
+++ foodPos/src/components/page/Pos.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="pos"> 
+		<el-row>
+			<el-col :span="7">
+				 <div class="hei order-list">24</div>
+			</el-col>
+			<el-col :span="17">
+				<div class="hei project-list">24</div>
+			</el-col> 
+		</el-row> 
+	</div>
+</template>
+
+<script>
+	export default({
+		name:'pos',
+		mounted:function(){
+			var orderListHeight = document.body.clientHeight;
+			var hei = document.getElementsByClassName('hei');
+			for(var i = 0;i < hei.length;i++){
+				hei[i].style.height = orderListHeight+'px';
+			} 
+		}
+	});
+</script>
+
+<style scoped>
+	.pos{overflow: hidden;}
+	.hei{width: 100%; background: #ddd;border: 1px solid red;} 
+</style>
